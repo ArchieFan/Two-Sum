4f30df6a0856bfc5927f0783918e0f9af2600ea1
Archie Fan <archiefan@gmail.com>
2022-06-30 08:37:09 +0100

    Add project files.

---
+++ Two Sum/Program.cs
@@ -0,0 +1,45 @@
+﻿public class Solution
+{
+    public int[] TwoSum(int[]? nums, int target)
+    {
+        if (nums == null) return new int[2];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i+1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return new int[2] { i, j};
+                }
+            }
+  
+        }
+        return new int[2];
+    }
+
+
+    static void Main()
+    {
+        //int[] nums = { 2, 7, 11, 15 };
+        //int target = 9;
+
+        int[] nums = { 3, 2, 4 };
+        int target = 6;
+
+        //int[] nums = { 3, 3 };
+        //int target = 6;
+
+        //int[] nums = { 8, 9, 2, 7, 11, 15 };
+        //int target = 9;
+
+        Solution s = new Solution();
+        int[] result = s.TwoSum(nums, target);
+        Console.WriteLine(String.Join(",", result));
+
+    }
+}
+
+
+
+
